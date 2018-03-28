ee3ceeebd7f2c502350c0bcad8467f9c3ff8e667
shan <shanweiliew93@gmail.com>
2018-03-28 16:39:08 +0800

    Testing product create with file upload images into alternatedocroot folder

--- Sportify-war/src/java/jsf/staff/managedbean/StaffProductManagedBean.java
+++ Sportify-war/src/java/jsf/staff/managedbean/StaffProductManagedBean.java
@@ -9,10 +9,14 @@ import ejb.session.stateless.ProductControllerLocal;
 import ejb.session.stateless.ProductSizeControllerLocal;
 import entity.Product;
 import entity.ProductSize;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
@@ -184,6 +188,43 @@ public class StaffProductManagedBean  implements Serializable{
             FacesContext.getCurrentInstance().addMessage(null, message);
         }*/
     } 
+    public void handleFileUpload_create(FileUploadEvent event){
+        try{
+            String newFilePath = FacesContext.getCurrentInstance().getExternalContext().getInitParameter("alternatedocroot_1") + System.getProperty("file.separator") + event.getFile().getFileName();
+            System.out.println("File Path: " + newFilePath);
+
+            File file = new File(newFilePath);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            int a;
+            int BUFFER_SIZE = 8192;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            InputStream inputStream = event.getFile().getInputstream();
+
+            while (true)
+            {
+                a = inputStream.read(buffer);
+
+                if (a < 0)
+                {
+                    break;
+                }
+
+                fileOutputStream.write(buffer, 0, a);
+                fileOutputStream.flush();
+            }
+
+            fileOutputStream.close();
+            inputStream.close();
+            
+             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,  "File uploaded successfully", ""));
+        }catch(Exception ex){
+            ex.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,  "File upload error: " + ex.getMessage(), ""));
+        }
+        
+    }
     public void deleteProduct(){
         System.out.println("To delete Product id: " + selectedProductToDelete.getId());
         System.out.println("Before deleting size is : " + products.size());
